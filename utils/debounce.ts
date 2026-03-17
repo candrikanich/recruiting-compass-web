e5adad8242a8b8d171cfc79a58b019736d444e0b
Chris Andrikanich <chris@andrikanich.com>
2026-03-17 19:14:35 -0400

    fix: widen debounce generic constraint to accept typed async functions

--- utils/debounce.ts
+++ utils/debounce.ts
@@ -1,4 +1,5 @@
-export function debounce<T extends (...args: unknown[]) => unknown>(
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function debounce<T extends (...args: any[]) => any>(
   fn: T,
   delay: number,
 ): (...args: Parameters<T>) => void {
